80a6eb7de698d5e7314cd3b33e3352be3c309203
Ian Gloude <igloude@gmail.com>
2016-05-30 19:41:01 -0500

    1.0.0

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 #!/usr/bin/env node
 
-var https = require("https");
-var cities = require("cities");
+var https = 	require("https"),
+	cities = 	require("cities");
 
 var zip = process.argv.slice(2);
 
-// allow multiple inputs
-
-// throw error when no argv
+if (zip.length > 0) {
+	weather(zip);
+} else {
+	console.log("err: no argument passed.");
+}
 
 function printer(data) {
 	console.log("");
-	console.log("");
+	// console.log("");
 	console.log(cities.zip_lookup(zip).city + ", " + cities.zip_lookup(zip).state_abbr + " " + zip);
 	console.log("");
 	console.log("Currently:");
@@ -20,7 +22,6 @@ function printer(data) {
 	console.log("Feels like " + data.currently.apparentTemperature + "\u00b0F");
 	console.log("Humidity: " + data.currently.humidity);
 	console.log("Wind: " + data.currently.windSpeed + " bearing \u00b0" + data.currently.windBearing);
-	// change bearing to cardinal directions
 	console.log("");
 	console.log(data.daily.summary);
 	console.log("");
@@ -29,6 +30,7 @@ function printer(data) {
 	console.log("High: " + data.daily.data[0].temperatureMax + "\u00b0F");
 	console.log("Low: " + data.daily.data[0].temperatureMin + "\u00b0F");
 	console.log("Precipitation: " + (data.daily.data[0].precipProbability * 100) + "%");
+	console.log("");
 }
 
 function weather(zip) {
@@ -36,6 +38,7 @@ function weather(zip) {
 	var longitude = cities.zip_lookup(zip).longitude;
 
 	var request = https.get('https://api.forecast.io/forecast/2a65c574d33b0f4ea0c5dda6b777c91c/' + latitude + ',' + longitude, function(response) {
+
 		var body = "";
 		response.on('data', function(chunk) {
 			body += chunk;
@@ -49,8 +52,7 @@ function weather(zip) {
 					console.error(error.message);
 				}
 			} else {
-				console.error("Oh no! Error.");
-				// custom messages for common statusCodes
+				console.error("Oh no! Error " + response.statusCode);
 			}
 		});
 	});
@@ -58,5 +60,3 @@ function weather(zip) {
 		console.error(error.message);
 	});
 }
-
-weather(zip);
